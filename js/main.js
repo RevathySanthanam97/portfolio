98e96c6f13da4150d2c0207cb60bfbfd9e36d605
RevathySanthanam97 <revathysanthanam97@gmail.com>
2021-06-21 12:39:12 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,54 @@
+window.onload = function(){ 
+    setTimeout(() => {
+        document.getElementById("preloader").classList.add('active');
+    }, 500);
+}
+document.getElementById('menu').addEventListener('click', function(){
+    document.getElementById('menuList').style.transform = "translateY(0%)";
+    document.getElementById('menuList').style.transition = "0.4s";
+})
+document.getElementById('menuClose').addEventListener('click', function(){
+    document.getElementById('menuList').style.transform = "translateY(-100%)";
+    document.getElementById('menuList').style.transition = "0.4s";
+})
+
+document.getElementById('color-changer').addEventListener('click', function(){
+    document.getElementById('color-changer').classList.toggle('active');
+})
+
+document.querySelectorAll(".selectColor").forEach(element => {
+    element.addEventListener("click", event => {
+       var selectedColor = element.getAttribute('title');
+       document.documentElement.style.setProperty('--color', selectedColor);
+    })
+})
+
+// Create the observer
+const observer = new IntersectionObserver(entries => {
+    console.log('arrived')
+  });
+  
+  // Tell the observer which elements to track
+observer.observe(document.querySelector('.skills'));
+
+document.querySelectorAll(".buttons").forEach(element => {
+    element.addEventListener("click", event => {
+       var selected = element.getAttribute('data-text');
+       document.querySelectorAll(".buttonSection").forEach(element => {
+            document.getElementById('breaker').style.display = "none";
+            element.style.display="none";
+            if(element.getAttribute('data-text') == selected){
+                setTimeout(() => {
+                    document.getElementById('menuList').style.transform = "translateY(-100%)";
+                    document.getElementById('menuList').style.transition = "0s";
+                    document.getElementById('breaker').style.display = "block";
+                }, 100);
+                setTimeout(() => {
+                    element.style.display="block";
+                },500)
+            }
+
+       })
+
+    });
+});
