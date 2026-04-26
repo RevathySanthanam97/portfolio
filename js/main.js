60c4e3085b414ccd93329c5e3014a7287ba73d60
RaySanthanam <raysanthanam@thoughtworks.com>
2026-04-26 13:52:43 +0530

    Theme Button Outside

--- js/main.js
+++ js/main.js
@@ -17,25 +17,20 @@ document.getElementById("menuClose").addEventListener("click", function () {
 });
 
 // Theme Update
-const themeSelectorElement = document.querySelectorAll(".theme-selector");
-const theme = localStorage.getItem("theme") || "light";
+const toggleBtn = document.getElementById("themeToggle");
 
-document.documentElement.setAttribute("data-theme", theme);
-themeSelectorElement.forEach(el => {
-  el.classList.toggle("active", el.dataset.themeOption === theme);
-});
+function setTheme(theme) {
+  document.documentElement.setAttribute("data-theme", theme);
+  localStorage.setItem("theme", theme);
+}
 
-themeSelectorElement.forEach(el => {
-  el.onclick = () => {
-    const selectedTheme = el.dataset.themeOption;
+const savedTheme = localStorage.getItem("theme") || "light";
+setTheme(savedTheme);
 
-    document.documentElement.setAttribute("data-theme", selectedTheme);
-    localStorage.setItem("theme", selectedTheme);
-
-    themeSelectorElement.forEach(e =>
-      e.classList.toggle("active", e === el)
-    );
-  };
+toggleBtn.addEventListener("click", () => {
+  const current = document.documentElement.getAttribute("data-theme");
+  const newTheme = current === "light" ? "dark" : "light";
+  setTheme(newTheme);
 });
 
 // Color Update
